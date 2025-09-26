5f0b26f79a25d6e5a70391fdf7e49aaba3b6f385
wawansakti1111 <wawanhola7@gmail.com>
2025-09-27 01:03:23 +0700

    revisi

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -11,18 +11,18 @@ class Kelas extends Model
 {
     use HasFactory;
 
-    // Nama tabel di database adalah 'classes', jadi kita perlu spesifikasikan
     protected $table = 'classes';
-
     protected $fillable = ['name', 'user_id'];
 
     public function students(): HasMany
     {
-        return $this->hasMany(User::class);
+        // Menggunakan foreign key yang benar
+        return $this->hasMany(User::class, 'class_id');
     }
 
     public function homeroomTeacher(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
 }
--- app/Models/Sdg.php
+++ app/Models/Sdg.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Relations\BelongsToMany; // <-- Pastikan ini ada
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +10,8 @@ class Sdg extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description'];
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'project_sdg');
+    }
 }
